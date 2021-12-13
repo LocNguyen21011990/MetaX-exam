2be79ea2d8c932274bed169ed99290807d54950d
Loc Nguyen <loc.nguyen@exe.com.vn>
2021-12-14 01:54:15 +0700

    fix bugs

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,20 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import { SessionProvider } from "next-auth/react"
 import theme from '../theme'
 import { AppProps } from 'next/app'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
-import React from 'react'
-
-const client = new ApolloClient({
-  uri: 'http://localhost:2101/graphql',
-  cache: new InMemoryCache(),
-  credentials: 'include'
-})
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../lib/apolloClient'
+import { SessionProvider } from 'next-auth/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps);
   return (
     <SessionProvider>
-      <ApolloProvider client={client}>
+      <ApolloProvider client={apolloClient}>
         <ChakraProvider resetCSS theme={theme}>
         <Component {...pageProps} />
         </ChakraProvider>
